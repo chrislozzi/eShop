a52199730f1ef2c0847c2d46f8c76f79979f851c
chrislozzi <mounede7@gmail.com>
2022-05-05 10:45:02 +0200

    gendiv

--- testShop.js
+++ testShop.js
@@ -1,7 +1,4 @@
 // Initialise la map des articl
-function init() {
-
-}
 class Article {
   constructor(pic, name, brand, category, price) {
     this.pic = pic;
@@ -11,11 +8,11 @@ class Article {
     this.price = price;
   }
 }
-let articles = [];
+ let articles=[];
 const iphone1 = new Article("/files/iphone.png", "Iphone 1", "Apple", "Hitech", 800);
-const Mec_keyboard = new Article("files/keyboard/.png", "Mechanical keyboard", "Blackmagic Design", "Hitech", 1200);
+const Mec_keyboard = new Article("/files/keyboard.png", "Mechanical keyboard", "Blackmagic Design", "Hitech", 1200);
 const macbook2 = new Article("/files/macbook.png", "Macbook 2", "Apple", "Hitech", 680);
-const hdd_corsair1 = new Article("/files/hdd.png", "HDD Corsair1", "Corsair", "Hitech", 100);
+const hdd_corsair1 = new Article("/files/hdd.png", "HDD Corsair1", "Corsair", "Hitech", 800);
 articles.push(iphone1, Mec_keyboard, macbook2, hdd_corsair1);
 /* const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
 const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
@@ -24,24 +21,19 @@ const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
 const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
 const Iphone1 = new Article(".png","Iphone 1","Hitech",800); */
 
-function genDiv() {
+function genDiv(){
+mySection = document.getElementById('container-liste-article')
+articles.forEach(art => {
 
+maDiv = document.createElement('div');
+maDiv.id = 'container-one-article';
+myImg = document.createElement('img');
+myImg.id = 'container-img';
+maDiv.style.border = '1px solid black'; //Pour mettre un border à ta div, par exemple
+myImg.setAttribute('src',art.img);
 
-  articles.forEach(art => {
-    myDiv = document.createElement('div');
-    myDiv.id = 'article-container';
-    myImg = document.createElement('img');
-    myImg.id = 'img-article'
-    myImg.setAttribute("src", art.pic);
-    maDiv.onclick = function () {
-      alert('Ajouté au panier');
-    }; //Évènement ayant lieu lors du click sur la div
-    maDiv.appendChild(myImg);
-    document.body.appendChild(maDiv);
-
-  });
-}
-/*************tentative d'appel de la fonction au chargement de la page************/
-document.addEventListener('DOMContentLoaded', function genDiv() {
-
-});
+maDiv.onclick = function(){alert('click !');}; //Évènement ayant lieu lors du click sur la div
+maDiv.appendChild(myImg);
+mySection.appendChild(maDiv);
+/* maDiv.appendChild(myImg); */
+});}
