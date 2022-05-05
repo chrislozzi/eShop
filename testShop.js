e2a71ffdd94fcad8079d68b83cf58c2e37db61fd
chrislozzi <mounede7@gmail.com>
2022-05-05 09:14:50 +0200

    firstcommit

---
+++ testShop.js
@@ -0,0 +1,47 @@
+// Initialise la map des articl
+function init() {
+
+}
+class Article {
+  constructor(pic, name, brand, category, price) {
+    this.pic = pic;
+    this.name = name;
+    this.brand = brand;
+    this.category = category;
+    this.price = price;
+  }
+}
+let articles = [];
+const iphone1 = new Article("/files/iphone.png", "Iphone 1", "Apple", "Hitech", 800);
+const Mec_keyboard = new Article("files/keyboard/.png", "Mechanical keyboard", "Blackmagic Design", "Hitech", 1200);
+const macbook2 = new Article("/files/macbook.png", "Macbook 2", "Apple", "Hitech", 680);
+const hdd_corsair1 = new Article("/files/hdd.png", "HDD Corsair1", "Corsair", "Hitech", 100);
+articles.push(iphone1, Mec_keyboard, macbook2, hdd_corsair1);
+/* const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
+const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
+const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
+const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
+const Iphone1 = new Article(".png","Iphone 1","Hitech",800);
+const Iphone1 = new Article(".png","Iphone 1","Hitech",800); */
+
+function genDiv() {
+
+
+  articles.forEach(art => {
+    myDiv = document.createElement('div');
+    myDiv.id = 'article-container';
+    myImg = document.createElement('img');
+    myImg.id = 'img-article'
+    myImg.setAttribute("src", art.pic);
+    maDiv.onclick = function () {
+      alert('Ajouté au panier');
+    }; //Évènement ayant lieu lors du click sur la div
+    maDiv.appendChild(myImg);
+    document.body.appendChild(maDiv);
+
+  });
+}
+/*************tentative d'appel de la fonction au chargement de la page************/
+document.addEventListener('DOMContentLoaded', function genDiv() {
+
+});
